a3bf7e628bd9b8a7b1344fe7aa6066fdc16bfe9c
protocol7 <me@proto.cool>
2025-01-27 12:12:10 -0700

    move site configuration to Payload globals

---
+++ src/globals/site.ts
@@ -0,0 +1,127 @@
+import type { GlobalConfig } from "payload";
+
+export const SiteGlobals: GlobalConfig = {
+    slug: "site",
+    label: "Core Site Settings",
+    admin: {
+        group: "Site Settings",
+    },
+    fields: [
+        {
+            name: "name",
+            label: "Site Name",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "author",
+            label: "Author Name",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "title",
+            label: "Site Title",
+            type: "text",
+            required: true,
+        },
+        {
+            name: "description",
+            label: "Site Description",
+            type: "textarea",
+            required: true,
+        },
+        {
+            name: "nav",
+            label: "Navigation Links",
+            type: "array",
+            fields: [
+                {
+                    name: "label",
+                    type: "text",
+                    required: true,
+                },
+                {
+                    name: "link",
+                    type: "text",
+                    required: true,
+                },
+            ],
+        },
+        {
+            name: "social",
+            label: "Social Links",
+            type: "array",
+            fields: [
+                {
+                    name: "platform",
+                    options: [
+                        "behance",
+                        "bluesky",
+                        "codepen",
+                        "dev",
+                        "discord",
+                        "dribbble",
+                        "email",
+                        "facebook",
+                        "github",
+                        "instagram",
+                        "linkedin",
+                        "mastodon",
+                        "medium",
+                        "pinterest",
+                        "producthunt",
+                        "reddit",
+                        "slack",
+                        "snapchat",
+                        "telegram",
+                        "threads",
+                        "tiktok",
+                        "twitch",
+                        "whatsapp",
+                        "youtube",
+                    ],
+                    required: true,
+                    type: "select",
+                },
+                {
+                    name: "label",
+                    type: "text",
+                    required: true,
+                },
+                {
+                    name: "link",
+                    type: "text",
+                    required: true,
+                },
+            ],
+        },
+        {
+            name: "site-settings",
+            label: "Site Settings",
+            type: "group",
+            admin: {
+                position: "sidebar",
+            },
+            fields: [
+                {
+                    name: "enable-builds",
+                    label: "Enable Builds (for GH/CF Pages, etc)",
+                    type: "checkbox",
+                    defaultValue: false,
+                },
+                {
+                    name: "use-view-transitions",
+                    label: "Use View Transition API",
+                    type: "checkbox",
+                    defaultValue: true,
+                },
+                {
+                    name: "use-animations",
+                    type: "checkbox",
+                    defaultValue: true,
+                },
+            ],
+        },
+    ],
+};
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -17,6 +17,7 @@ import { Media } from "./collections/core/Media";
 import { Posts } from "./collections/blog/Posts";
 import { Tags } from "./collections/blog/Tags";
 import Projects from "@/collections/content/Projects";
+import { SiteGlobals } from "@/globals/site";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
@@ -29,6 +30,7 @@ export default buildConfig({
         },
     },
     collections: [Tags, Posts, Media, Users, Projects],
+    globals: [SiteGlobals],
     cors: {
         origins: ["http://localhost:3000", "https://payload.proto.cool", "https://proto.cool"],
     },
