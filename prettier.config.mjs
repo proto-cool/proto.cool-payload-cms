ab9160fe09e599a33dcf710098b27dafb9e33634
protocol7 <me@proto.cool>
2025-02-05 09:50:31 -0700

    update prettier config to ESM config.mjs

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+export default {
+    semi: true,
+    tabWidth: 4,
+    useTabs: false,
+    singleQuote: false,
+    trailingComma: "all",
+    printWidth: 100,
+    endOfLine: "lf",
+};
