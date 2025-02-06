0829811101e1f2ac6e2b8f99425e4202f0a403fe
protocol7 <me@proto.cool>
2025-02-06 12:52:47 -0700

    oops, need to set the env var in the client as well

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "standalone",
+    env: {
+        FRONTEND_BUILD_URL: process.env.FRONTEND_BUILD_URL,
+    },
 };
 
 export default withPayload(nextConfig);
